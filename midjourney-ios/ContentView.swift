9f751084284cc1ea649c87a770da4f24ca569145
danielamitay <hello@danielamitay.com>
2023-12-20 14:42:32 -0500

    Initial project setup

---
+++ midjourney-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  midjourney-ios
+//
+//  Created by Daniel Amitay on 12/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ midjourney-ios/MidjourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidjourneyApp.swift
+//  midjourney-ios
+//
+//  Created by Daniel Amitay on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MidjourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
