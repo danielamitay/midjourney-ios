802a5994052e1512ad6244a044aac17f7b560355
danielamitay <hello@danielamitay.com>
2023-12-20 17:16:50 -0500

    Start working on the bottom tab bar

---
+++ midjourney-ios/MyJobsView.swift
@@ -0,0 +1,92 @@
+//
+//  MyJobsView.swift
+//  midjourney-ios
+//
+//  Created by Daniel Amitay on 12/20/23.
+//
+
+import SwiftUI
+
+import Kingfisher
+import Midjourney
+
+struct MyJobsView: View {
+    let client: Midjourney
+    let userId: String
+
+    @State var myJobs: [Midjourney.MyJob] = []
+
+    private let gridCount: Int = 4
+    private let gridPadding: CGFloat = 3
+    private let gridCorners: CGFloat = 3
+
+    var body: some View {
+        ScrollView {
+            HStack {
+                Text("Today")
+                    .bold()
+                Spacer()
+            }
+            .padding(.horizontal, 27)
+            .frame(height: 70)
+            LazyVGrid(columns: columns, spacing: gridPadding) {
+                ForEach(myJobs) { job in
+                    Color(white: 0.913)
+                        .aspectRatio(1, contentMode: .fit)
+                        .overlay {
+                            KFImage.url(job.imageUrl)
+                                .resizable()
+                                .loadDiskFileSynchronously()
+                                .fade(duration: 0.25)
+                                .aspectRatio(contentMode: .fill)
+                                .clipped()
+                                .disabled(true)
+                        }
+                }
+                .clipShape(
+                    RoundedRectangle(cornerRadius: gridCorners)
+                )
+            }
+            .padding(gridPadding)
+        }
+        .onAppear {
+            client.myJobs(userId: userId) { result in
+                self.onResult(result)
+            }
+        }
+    }
+
+    func onResult(_ result: Result<[Midjourney.MyJob], Error>) {
+        switch result {
+        case .success(let success):
+            myJobs = success
+        case .failure(_):
+            myJobs = []
+        }
+    }
+}
+
+extension MyJobsView {
+    var columns: [GridItem] {
+        var returnValue: [GridItem] = []
+        for _ in 0..<gridCount {
+            returnValue.append(.init(.flexible(), spacing: gridPadding))
+        }
+        return returnValue
+    }
+}
+
+extension Midjourney.MyJob {
+    var imageUrl: URL {
+        return URL(string: "https://cdn.midjourney.com/\(id)/0_0_384_N.webp?method=shortest&qst=6&quality=50")!
+    }
+}
+
+/*
+#Preview {
+    MyJobsView(
+        client: .init(cookie: ""),
+        userId: ""
+    )
+}
+*/
--- midjourney-ios/SignedInView.swift
+++ midjourney-ios/SignedInView.swift
@@ -14,62 +14,43 @@ struct SignedInView: View {
     let client: Midjourney
     let userId: String
 
+    enum Tab {
+        case explore
+        case myImages
+    }
+
+    @State var selectedTab: Tab = .explore
     @State var myJobs: [Midjourney.MyJob] = []
 
-    private let gridCount: Int = 4
-    private let gridPadding: CGFloat = 3
-    private let gridCorners: CGFloat = 3
-
-    var columns: [GridItem] {
-        var returnValue: [GridItem] = []
-        for _ in 0..<gridCount {
-            returnValue.append(.init(.flexible(), spacing: gridPadding))
-        }
-        return returnValue
-    }
-
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: gridPadding) {
-                ForEach(myJobs) { job in
-                    Color(white: 0.913)
-                        .aspectRatio(1, contentMode: .fit)
-                        .overlay {
-                            KFImage.url(job.imageUrl)
-                                .resizable()
-                                .loadDiskFileSynchronously()
-                                .fade(duration: 0.25)
-                                .aspectRatio(contentMode: .fill)
-                                .clipped()
-                                .disabled(true)
+        ZStack {
+            Color.gray
+                .opacity(0.5)
+                .ignoresSafeArea()
+                .opacity(selectedTab == .explore ? 1.0 : 0.0)
+
+            MyJobsView(client: client, userId: userId)
+                .opacity(selectedTab == .myImages ? 1.0 : 0.0)
+
+            VStack {
+                Spacer()
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.white)
+                    .stroke(.gray.opacity(0.5), lineWidth: 1)
+                    .frame(height: 62)
+                    .padding(.horizontal, 12)
+                    .overlay {
+                        HStack {
+                            Button("Explore") {
+                                selectedTab = .explore
+                            }
+                            Button("My Images") {
+                                selectedTab = .myImages
+                            }
                         }
-                }
-                .clipShape(
-                    RoundedRectangle(cornerRadius: gridCorners)
-                )
-            }
-            .padding(gridPadding)
-        }
-        .onAppear {
-            client.myJobs(userId: userId) { result in
-                self.onResult(result)
+                    }
             }
         }
-    }
-
-    func onResult(_ result: Result<[Midjourney.MyJob], Error>) {
-        switch result {
-        case .success(let success):
-            myJobs = success
-        case .failure(_):
-            myJobs = []
-        }
-    }
-}
-
-extension Midjourney.MyJob {
-    var imageUrl: URL {
-        return URL(string: "https://cdn.midjourney.com/\(id)/0_0_384_N.webp?method=shortest&qst=6&quality=50")!
     }
 }
 
