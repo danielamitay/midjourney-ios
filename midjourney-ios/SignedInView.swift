6ebedfc4998feabf90d352638cd25d342b000633
danielamitay <hello@danielamitay.com>
2023-12-20 22:33:02 -0500

    Bring in correct colors and icons for the tab bar

--- midjourney-ios/SignedInView.swift
+++ midjourney-ios/SignedInView.swift
@@ -24,8 +24,11 @@ struct SignedInView: View {
 
     var body: some View {
         ZStack {
+            Color.background
+                .ignoresSafeArea()
+
             Color.gray
-                .opacity(0.5)
+                .opacity(0.25)
                 .ignoresSafeArea()
                 .opacity(selectedTab == .explore ? 1.0 : 0.0)
 
@@ -35,20 +38,55 @@ struct SignedInView: View {
             VStack {
                 Spacer()
                 RoundedRectangle(cornerRadius: 10)
-                    .fill(.white)
-                    .stroke(.gray.opacity(0.5), lineWidth: 1)
+                    .fill(.menu)
+                    .stroke(.menuBorder, lineWidth: 1)
                     .frame(height: 62)
-                    .padding(.horizontal, 12)
                     .overlay {
                         HStack {
-                            Button("Explore") {
-                                selectedTab = .explore
-                            }
-                            Button("My Images") {
-                                selectedTab = .myImages
-                            }
+                            Color.clear
+                                .aspectRatio(1, contentMode: .fit)
+                                .overlay {
+                                    Image(.menuIcon)
+                                        .foregroundStyle(.standardText)
+                                }
+                            let exploreButtonColor = selectedTab == .explore ? Color.selectedText.opacity(0.1) : Color.menu
+                            let exploreTextColor = selectedTab == .explore ? Color.selectedText : Color.deselectedText
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(exploreButtonColor)
+                                .stroke(exploreButtonColor.opacity(0.1), lineWidth: 1)
+                                .onTapGesture {
+                                    selectedTab = .explore
+                                }
+                                .overlay {
+                                    HStack(spacing: 5) {
+                                        Image(.compassIcon)
+                                        Text("Explore")
+                                            .font(.system(size: 15))
+                                            .fontWeight(.medium)
+                                    }
+                                    .foregroundStyle(exploreTextColor)
+                                }
+                            let myImagesButtonColor = selectedTab == .myImages ? Color.selectedText.opacity(0.1) : Color.menu
+                            let myImagesTextColor = selectedTab == .myImages ? Color.selectedText : Color.deselectedText
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(myImagesButtonColor)
+                                .stroke(myImagesButtonColor, lineWidth: 1)
+                                .onTapGesture {
+                                    selectedTab = .myImages
+                                }
+                                .overlay {
+                                    HStack(spacing: 2) {
+                                        Image(.photoIcon)
+                                        Text("My Images")
+                                            .font(.system(size: 15))
+                                            .fontWeight(.medium)
+                                    }
+                                    .foregroundStyle(myImagesTextColor)
+                                }
                         }
+                        .padding(8)
                     }
+                    .padding(.horizontal, 12)
             }
         }
     }
